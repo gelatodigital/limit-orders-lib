47540015f203029a7025f07bd9a14347db7bc535
Shivva <matthieumariejoseph@gmail.com>
2021-04-26 18:49:32 +0530

    Remove cancelLimitOrder function

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import { TransactionResponse } from "@ethersproject/abstract-provider";
 import {
   BigNumber,
   constants,
-  ContractTransaction,
   getDefaultProvider,
   Signer,
   utils,
@@ -166,31 +165,6 @@ export const sendLimitOrder = async (
 //#endregion Limit Orders Submission
 
 //#region Limit Orders Cancellation
-
-export const cancelLimitOrder = async (
-  signer: Signer,
-  fromCurrency: string,
-  toCurrency: string,
-  minReturn: BigNumber,
-  witness: string
-): Promise<ContractTransaction> => {
-  if (!signer.provider)
-    throw new Error("getLimitOrderPayloadWithSecret: no signer on signer");
-
-  const chainId = (await signer.provider?.getNetwork()).chainId;
-  const provider = await getDefaultProvider(getNetworkName(chainId));
-  const abiCoder = new utils.AbiCoder();
-
-  const gelatoPineCore = await getGelatoPineCore(provider);
-
-  return gelatoPineCore.cancelOrder(
-    await getLimitOrderModuleAddr((await signer.provider.getNetwork()).chainId),
-    fromCurrency,
-    await signer.getAddress(),
-    witness,
-    abiCoder.encode(["address", "uint256"], [toCurrency, minReturn])
-  );
-};
 
 export const getCancelLimitOrderPayload = async (
   chainId: number,
