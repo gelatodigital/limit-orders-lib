b18d11d07d8143d1d2b04beeb3df59c004c6e94d
Twin Fish <matthieumariejoseph@gmail.com>
2021-05-14 14:10:39 +0530

    sort order by creation date (#20)
    * sort order by creation date
    
    * Query name camel case

--- src/query/graphql.ts
+++ src/query/graphql.ts
@@ -2,7 +2,7 @@ import { gql } from "graphql-request";
 
 export const GET_ALL_ORDERS_BY_OWNER = gql`
   query getOrdersByOwner($owner: String) {
-    orders(first: 1000, where: { owner: $owner }) {
+    orders(first: 1000, orderBy: updatedAt, orderDirection: desc, where: { owner: $owner }) {
       id
       owner
       inputToken
@@ -20,9 +20,10 @@ export const GET_ALL_ORDERS_BY_OWNER = gql`
   }
 `;
 
+
 export const GET_ALL_OPEN_ORDERS_BY_OWNER = gql`
   query getOpenOrdersByOwner($owner: String) {
-    orders(first: 1000, where: { owner: $owner, status: open }) {
+    orders(first: 1000, orderBy: updatedAt, orderDirection: desc, where: { owner: $owner, status: open }) {
       id
       owner
       inputToken
@@ -40,7 +41,7 @@ export const GET_ALL_OPEN_ORDERS_BY_OWNER = gql`
 
 export const GET_ALL_PAST_ORDERS_BY_OWNER = gql`
   query getPastOrdersByOwner($owner: String) {
-    orders(first: 1000, where: { owner: $owner, status_not: open }) {
+    orders(first: 1000, orderBy: updatedAt, orderDirection: desc, where: { owner: $owner, status_not: open }) {
       id
       owner
       inputToken
@@ -60,7 +61,7 @@ export const GET_ALL_PAST_ORDERS_BY_OWNER = gql`
 
 export const GET_ALL_EXECUTED_ORDERS_BY_OWNER = gql`
   query getExecutedOrdersByOwner($owner: String) {
-    orders(first: 1000, where: { owner: $owner, status: executed }) {
+    orders(first: 1000, orderBy: updatedAt, orderDirection: desc, where: { owner: $owner, status: executed }) {
       id
       owner
       inputToken
@@ -79,7 +80,7 @@ export const GET_ALL_EXECUTED_ORDERS_BY_OWNER = gql`
 
 export const GET_ALL_CANCELLED_ORDERS_BY_OWNER = gql`
   query getCancelledOrdersByOwner($owner: String) {
-    orders(first: 1000, where: { owner: $owner, status: cancelled }) {
+    orders(first: 1000, orderBy: updatedAt, orderDirection: desc, where: { owner: $owner, status: cancelled }) {
       id
       owner
       inputToken
