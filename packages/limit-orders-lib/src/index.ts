c38c743579338a5c84cf5c81122597386342429a
Pedro Cruz <pedrocrvz@gmail.com>
2021-06-07 15:38:13 +0100

    chore: remove lib test

--- packages/limit-orders-lib/src/index.ts
+++ packages/limit-orders-lib/src/index.ts
@@ -1,6 +1,3 @@
-import { BigNumber, utils, Wallet } from "ethers";
-import { queryOpenOrders } from "./utils/queries";
-
 export { GelatoLimitOrders } from "./limit-orders";
 
 export * from "./types";
