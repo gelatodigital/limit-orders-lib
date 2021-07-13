ed05b18f711e54e4940b341ecd283641bcc4049e
Pedro Cruz <pedrocrvz@gmail.com>
2021-07-13 17:11:21 +0200

    chore(limit-orders-react): bump limit-orders-lib

--- packages/limit-orders-react/src/hooks/useApproveCallback.ts
+++ packages/limit-orders-react/src/hooks/useApproveCallback.ts
@@ -12,6 +12,7 @@ import { useTokenContract } from "./useContract";
 import { useWeb3 } from "../web3";
 import { useTokenAllowance } from "./useTokenAllowance";
 import useGelatoLimitOrdersLib from "./gelato/useGelatoLimitOrdersLib";
+import { GELATO_LIMIT_ORDERS_MODULE_ADDRESS } from "@gelatonetwork/limit-orders-lib/dist/constants";
 
 export enum ApprovalState {
   UNKNOWN = "UNKNOWN",
@@ -132,6 +133,7 @@ export function useApproveCallbackFromInputCurrencyAmount(
   currencyAmountIn: CurrencyAmount<Currency> | undefined
 ) {
   const gelatoLibrary = useGelatoLimitOrdersLib();
+
   return useApproveCallback(
     currencyAmountIn,
     gelatoLibrary?.erc20OrderRouter.address ?? undefined
